f33eb9e8b1c6a862447c67e23593d530671f149a
ganbatte <vinokurovvin@list.ru>
2025-10-05 21:30:26 +0900

    prepare ui

--- web/admin/src/main.ts
+++ web/admin/src/main.ts
@@ -1,11 +1,19 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import '@/assets/style.css'
+import Aura from '@primeuix/themes/aura'
 
 const app = createApp(App)
 
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+  unstyled: true,
+})
 app.use(createPinia())
 app.use(router)
 
